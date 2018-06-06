6b33f04068928f3683e30d0da11147dcd1e73377
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-06 17:16:46 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
+
 		 programmer_hash[:grace_hopper]
 
 end
@@ -84,8 +85,8 @@ def adding_matz
         }
      }
 
-	programmer_hash[:yukihiro_matsumoto]={known_for: "Ruby", languages: ["LISP", "C"]}
-	programmer_hash
+		 programmer_hash[:yukihiro_matsumoto]={known_for: "Ruby", languages: ["LISP", "C"]}
+		 programmer_hash
 end
 
 def changing_alan
@@ -130,6 +131,6 @@ def adding_to_dennis
         }
      }
 
-  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-	programmer_hash
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash
 end
